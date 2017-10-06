9bb0b5ad1a757da5d28a9cb22283ab6bc487e53f
jeans <jean.stam@libertymutual.com>
2017-10-06 12:49:34 -0700

    Initial

---
+++ communityShed/src/main/java/com/libertymutual/goforcode/communityShed/CommunityShedApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.goforcode.communityShed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityShedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityShedApplication.class, args);
+	}
+}
