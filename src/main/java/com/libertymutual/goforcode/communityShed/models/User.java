03e2cfd5021f2fd1709ca224f61eed5459faafe6
Serena Zywicki <serena.zywicki@libertymutual.com>
2017-10-10 12:59:40 -0500

    User entity created

--- src/main/java/com/libertymutual/goforcode/communityShed/models/User.java
+++ src/main/java/com/libertymutual/goforcode/communityShed/models/User.java
@@ -1,22 +1,106 @@
-//package com.libertymutual.goforcode.communityShed.models;
-//
-//import java.util.List;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.SequenceGenerator;
-//
-//import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-//import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-//
-//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
-//@Entity
-//
-//public class User {
-//	@Id
-//	@GeneratedValue(generator = "UserIdSeq", strategy = GenerationType.AUTO)
-//	@SequenceGenerator(name = "UserIdSeq", sequenceName = "UserIdSeq")
-//	private Long id;
-//}
+package com.libertymutual.goforcode.communityShed.models;
+
+
+
+import java.security.acl.Group;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.Id;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(
+		generator = ObjectIdGenerators.PropertyGenerator.class, 
+		property = "id"
+)
+
+@Entity
+@Table(name="communityshed_user")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(length=75, nullable=false)
+	private String firstName;
+	
+	@Column(length=75, nullable=false)
+	private String lastName;
+	
+	@Column(unique=true, length=255, nullable=false)
+	private String email;
+	
+	@Column(length=255, nullable=false)
+	private String password;
+	
+	@ManyToMany(fetch=FetchType.EAGER, mappedBy="user", cascade=CascadeType.ALL)
+	private List<Group> groups;
+
+	public User() {}
+	
+	public User(String password, String email, String firstName, String lastName) {
+		this.password = password;
+		this.email = email;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<Group> groups) {
+		this.groups = groups;
+	}
+}
