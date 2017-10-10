8f627663a39d49a782403c46dc114bca3b3155df
Serena Zywicki <serena.zywicki@libertymutual.com>
2017-10-10 17:06:16 -0500

    added userApiController

---
+++ src/test/java/com/libertymutual/goforcode/communityShed/api/UserApiControllerTests.java
@@ -0,0 +1,14 @@
+package com.libertymutual.goforcode.communityShed.api;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserApiControllerTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
